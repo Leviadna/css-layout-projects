03479bc0648e97027c05a9d24482e0f6a361be8b
Leviadna <79688185+Leviadna@users.noreply.github.com>
2024-08-02 17:30:31 +0900

    like page

--- js/category.js
+++ js/category.js
@@ -1,7 +1,7 @@
-$(function(){
+$(()=>{
+  const $link = $('.link_snb');
   let ww = $(window).width();
   let mySwiper = undefined;
-  let panelHeight = $('.article-panel').height();
 
   // 해상도 530 미만에서만 swiper 작동
   function initSwiper(){
@@ -16,55 +16,53 @@ $(function(){
     }
   }
   initSwiper();
+  
+  $(window).on('scroll', function(){
+    var _sct = $(window).scrollTop();
 
-  // 좌측 카테고리 클릭 시 해당하는 카테고리의 서브 메뉴 Top 위치로 이동
-  function react(){
-    for(let i=0; i<14; i++){
-      $('.btn_tablist:nth-of-type('+i+')').on("click",function(){
-        $('html, body').animate({
-          scrollTop : panelHeight*(i-1)
-        }, 300)
-      });
-    }
-  }
-  react();
+    // 스크롤시 탭메뉴에 해당하는 영역 나오면 링크메뉴 속성 변경
+    $link.each(function(){
+      var thisHash = $(this.hash);
 
-  // 스크롤 위치에 따른 좌측 카테고리 버튼 on class 부여
-  $(window).scroll(function(){
-    for(let i=0; i<14; i++){
-      if($(this).scrollTop() > panelHeight*(i-1) - (panelHeight/3)){
-        $('.btn_tablist').removeClass('on');
-        $('.btn_tablist:nth-of-type('+i+')').addClass('on');
+      if(thisHash.length){
+        var thisOffset = thisHash.offset().top - 180;
+        if(_sct >= thisOffset - 1){
+          $link.attr('aria-selected', 'false');
+          $(this).attr('aria-selected', 'true');
+        }
       }
-    }
+    });
   });
 
-  // 해상도 변경될 때마다 resize 시키기
-  $(window).resize(function(){
-    ww = $(window).width();
-    initSwiper();
-    react();
+  // 플로팅탭 클릭시 이동
+  $link.on('click', function(e){
+    e.preventDefault();
+    $link.attr('aria-selected', 'false');
+    $(this).attr('aria-selected', 'true');
+    $('html, body').animate({
+      scrollTop: $(this.hash).offset().top - 180
+    }, 0);
   });
 
-  // 과부하 방지로 resize 후 새로고침
-  $(window).resize(function(){
-    document.location.reload();
+  // 검색창 클릭 시 searchModule on, 취소 버튼 클릭 시 searchModule off
+  $('.btn_txtbox').click(function(){
+    $('.container-doc').css('display','none');
+    $('#searchModule').addClass('on');
+  });
+  $('.search-header .btn_header').click(function(){
+    $('#searchModule').removeClass('on');
+    $('.container-doc').css('display','block');
   });
 
-  // 새로고침 시간 설정
-  let delay = 300;
-  let timer = null;
-  $(window).on('resize', function(){
-    clearTimeout(timer);
-    timer = setTimeout(function(){
-    document.location.reload();
-    }, delay);
-  });
-
-  // .btn_top 버튼 클릭 시 최상단으로 이동
+  // 클릭 시 최상단으로 이동
   $('.btn_top').on("click",function(){
     $('html, body').animate({
       scrollTop : 0
-    }, 100);
+    },100)
+  });
+
+  //검색 모듈 최근 검색 목록 swiper
+  new Swiper('.nav-search', {
+    slidesPerView:'auto'
   });
 });
